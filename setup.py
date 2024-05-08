64396d3c73d33365c08201ec7189544eb369072e
🧐 ᴛᴜꜱʜᴀʀ-ᴘᴇʀꜱᴘᴇᴄᴛɪᴠᴇ <61559891+TUSHAR-PERSPECTIVE@users.noreply.github.com>
2024-05-09 01:52:16 +0530

    3rd commit

--- setup.py
+++ setup.py
@@ -5,10 +5,12 @@ setup(
     version='1.0.0',
     packages=['picdiffbox'],
     install_requires=[
-        'PIL',  # Assuming you're using PIL for image processing
+        'opencv-python',
+        'scikit-image',
+        'numpy',
     ],
     author='Tushar Arora',
-    author_email='Tuhsar.aka.datascientist@example.com',
+    author_email='Tushar.aka.datascientist@gmail.com',
     description='A Python library to compare two images and draw bounding boxes around the differences.',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
@@ -21,5 +23,6 @@ setup(
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
     ],
 )
